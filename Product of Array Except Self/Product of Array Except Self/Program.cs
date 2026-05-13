d1fc1783134ca6a75f7f01767303b55da7f88605
Bhawna Rawat <rbhawna96@gmail.com>
2026-05-13 22:11:12 +0530

    Added selected DSA problems

---
+++ Product of Array Except Self/Product of Array Except Self/Program.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Linq;
+
+namespace ProductOfArrayExceptSelfConsoleApp
+{
+    class Program
+    {
+        static int[] ProductOfArray(int[] nums)
+        {
+            int n = nums.Length;
+            int[] result = new int[n];
+            result[0] = 1;
+            for(int i = 1; i < n; i++)
+            {
+                result[i] = result[i-1] * nums[i-1];
+            }
+
+            int right = 1;
+            for(int i = n-1; i >= 0; i--)
+            {
+                result[i] *= right;
+                right *= nums[i];
+            }
+
+            return result;
+        }
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Product of Array Except Self");
+            Console.Write("Enter array element separated by space: ");
+            int[] nums = Console.ReadLine()
+                                .Split(' ').Select(int.Parse).ToArray();
+
+            int[] result = ProductOfArray(nums);
+            Console.Write("Output: " + string.Join(" ", result));
+            Console.ReadLine();
+        }
+    }
+}
---
+++ Subarray Sum Equals K/Program.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Linq;
+namespace SubarraySumEqualsK
+{
+    class Program
+    {
+        static int SubArraySum(int[] nums , int k)
+        {
+            Dictionary<int, int> map = new Dictionary<int, int>();
+            int count = 0;
+            int sum = 0;
+            map[0] = 1;
+            foreach(int i in nums)
+            {
+                sum += i;
+                if (map.ContainsKey(sum - k))
+                    count += map[sum - k];
+                if(!map.ContainsKey(sum))
+                    map[sum] = 0;
+                map[sum]++;
+            }
+            return count;
+        }
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Contain Duplicate");
+            Console.Write("Enter array elements separated by space:");
+            int[] nums = Console.ReadLine().Split(' ').Select(int.Parse).ToArray();
+            Console.Write("Enter number for K :");
+            int k = int.Parse(Console.ReadLine());
+
+            int result = SubArraySum( nums, k);
+            Console.WriteLine("Output: " + result);
+            Console.ReadLine();
+        }
+    }
+}
