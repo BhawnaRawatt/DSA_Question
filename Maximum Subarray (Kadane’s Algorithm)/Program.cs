79e6de46ce81b49d0f51a58140faa4043a52f3fa
Bhawna Rawat <rbhawna96@gmail.com>
2026-05-13 22:28:54 +0530

    Second Commit

---
+++ Maximum Subarray (Kadane’s Algorithm)/Program.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Linq;
+
+namespace MaximumSubarray
+{
+    class Program
+    {
+        static int MaxSubArray(int[] nums)
+        {
+            int currentSum = nums[0];
+            int maxSum = nums[0];
+
+            for(int i = 1; i < nums.Length; i++)
+            {
+                currentSum = Math.Max(nums[i], currentSum + nums[i]);
+                maxSum = Math.Max(currentSum, maxSum);
+            }
+            return maxSum;
+        }
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Maximum Subarray (Kadane’s Algorithm)");
+            Console.Write("Enter array element separated by space: ");
+            int[] nums = Console.ReadLine()
+                                    .Split(' ').Select(int.Parse).ToArray();
+
+            int result = MaxSubArray(nums);
+            Console.Write("Output : "+ result);
+            Console.ReadLine();
+        }
+    }
+}
