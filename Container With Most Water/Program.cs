561887bace8bf061449616c556ee118bc32a4901
Bhawna Rawat <rbhawna96@gmail.com>
2026-05-15 21:54:15 +0530

    Fourth Commit

---
+++ Container With Most Water/Program.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Linq;
+
+namespace MostWaterConsoleApp
+{
+    class Program
+    {
+        static int MaxArea(int[] height)
+        {
+            int left = 0;
+            int right = height.Length - 1;
+            int MaxValue = 0;
+            while (left < right)
+            {
+                int h = Math.Min(height[right], height[left]);
+                int w = right - left;
+                MaxValue = Math.Max(MaxValue, h*w);
+                if (height[left] < height[right])
+                    left++;
+                else
+                    right--;
+            }
+            return MaxValue;
+        }
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Container With Most Water");
+            Console.Write("Enter a number with a space: ");
+            int[] num = Console.ReadLine().Split(' ').Select(int.Parse).ToArray();
+
+            int result = MaxArea(num);
+            Console.Write("Output: " + result);
+            Console.ReadLine();
+
+        }
+    }
+}
